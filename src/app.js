31b2a8b789a4a9f780db0c68e468173550055d26
Helena Paixão <hp.helenapaixao@gmail.com>
2020-04-06 19:42:22 -0300

    config app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,21 +10,38 @@ app.use(cors());
 
 const repositories = [];
 
+app.post("/repositories", (request, response) => {
+  const { title, techs, likes } = request.body;
+
+  const repository = { id: uuid(), title, techs, likes };
+
+  repositories.push(repository);
+  return response.json(repository);
+});
+
 app.get("/repositories", (request, response) => {
   // TODO
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
-});
-
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const {title,techs,likes} = request.body;
+
+  const respositorieIndex = respositories.findIndex(respositorie => repositorie.id === id);
+  if (respositorieIndex < 0) {
+    return response.status(400).json({ error: "Repositorie not found" });
+  }
+  const repositorie = {
+    id,
+    title,
+    techs,
+    likes,
+  }
+  repositories[repositorieIndex] = respositorie;
+  return response.json(repositorie);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
-});
+app.delete("/repositories/:id", (request, response) => {});
 
 app.post("/repositories/:id/like", (request, response) => {
   // TODO
